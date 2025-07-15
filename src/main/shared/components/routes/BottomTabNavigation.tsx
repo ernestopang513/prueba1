85198482e3b8d27bbd1c7b7c4e7f2c40c177f864
ErnestoLap <ernestopang513@hotmail.com>
2025-07-15 16:20:46 -0600

    feat: bottom icons

--- src/main/shared/components/routes/BottomTabNavigation.tsx
+++ src/main/shared/components/routes/BottomTabNavigation.tsx
@@ -5,20 +5,32 @@ import LoginScreen from '../../../Auth/presentation/screens/LoginScreen';
 import LogOutScreen from '../../../Auth/presentation/screens/LogOutScreen';
 import ProductNavigation from '../../../product/ProductNavigation';
 import FacturasScreen from '../../../estadoCuenta/presentation/screens/FacturasScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
+type IoniconName = keyof typeof Ionicons.glyphMap;
 
 const BottomTabNavigation = () => {
   return (
     <Tab.Navigator
-     screenOptions={{
-      // headerShown: false,
-      // tabBarIcon: () => null,
-     }}
+     screenOptions={({route}) => {
+        return ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName: IoniconName = 'help-outline'
+
+            if (route.name =="Home" ) {
+              iconName = focused ? "document-text" : "document-text-outline"
+            } else if (route.name == "Profile") {
+              iconName = focused ? "person" : "person-outline"
+            }
+            return <Ionicons name={iconName} size={size} color={color}  />
+          }
+        })
+     }} 
     >
         <Tab.Screen name = "Home"  component={ProductNavigation} options={{headerShown: false}} />
-        <Tab.Screen name = "Otro"  component={FacturasScreen}/>
-        <Tab.Screen name = "Logout"  component={LogOutScreen}/>
+        {/* <Tab.Screen name = "Otro"  component={FacturasScreen}/> */}
+        <Tab.Screen name = "Profile"  component={LogOutScreen}/>
     </Tab.Navigator>
   )
 }
